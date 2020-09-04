5567c83d226e3b1c9db80351f8871bf76f3f2f50
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-04 17:34:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,8 @@ end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-    planeteer_calls.map do |element|
-      element.any?length > 4
+    planeteer_calls.any? do |element|
+      element.length > 4
     end
   # Return the boolean result of this check
 end
